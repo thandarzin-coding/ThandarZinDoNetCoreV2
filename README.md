14dbfdf04624b2b77e38c8c05aec6e9f97e74e1f
thandarzin93 <tweetygirl2018@gmail.com>
2023-12-20 02:04:28 -0800

    [Api/Document]Complete

--- README.md
+++ README.md
@@ -6,3 +6,23 @@
     <PackageReference Include="Microsoft.EntityFrameworkCore.SqlServer" Version="7.0.14" />
     <PackageReference Include="Microsoft.EntityFrameworkCore.Tools" Version="7.0.14">
 
+    api
+
+http method
+-----------------
+get     => read
+post    => create
+put     => update
+patch   => update
+delete  => delete
+
+http status code
+-----------------
+200 - ok
+404 - not found
+500 - internal server error
+402, 403, 409
+
+100 - 199
+200 - 299
+300 - 399
---
+++ ThandarZinDoNetCore.RestApi/Controllers/BlogController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ThandarZinDoNetCore.RestApi;
+
+namespace ThandarZinDoNetCore.RestApi.Controllers
+{
+	//https://localhost:3000/api/blog
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BlogController : ControllerBase
+	{
+
+		[HttpGet]
+		public IActionResult GetBlogs()
+		{
+			return Ok("Get");
+		}
+
+		[HttpPost]
+		public IActionResult CreateBlogs()
+		{
+			return Ok("Post");
+		}
+
+		[HttpPut]
+		public IActionResult UpdateBlogs()
+		{
+			return Ok("Put");
+		}
+
+		[HttpPatch]
+		public IActionResult PatchBlogs()
+		{
+			return Ok("Patch");
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteBlogs()
+		{
+			return Ok("Delete");
+		}
+	}
+}
