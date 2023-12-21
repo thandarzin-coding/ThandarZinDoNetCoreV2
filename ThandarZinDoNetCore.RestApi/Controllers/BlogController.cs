a356960113b9618ab90ff30f5af397227a7bf0ae
thandarzin93 <tweetygirl2018@gmail.com>
2023-12-21 10:21:42 -0800

    [Complete]Page Size

--- ThandarZinDoNetCore.RestApi/Controllers/BlogController.cs
+++ ThandarZinDoNetCore.RestApi/Controllers/BlogController.cs
@@ -12,14 +12,43 @@ namespace ThandarZinDoNetCore.RestApi.Controllers
 	{
 		private readonly AppDbContext _dbContext = new AppDbContext();
 
-        [HttpGet]
+		[HttpGet]
 		public IActionResult GetBlogs()
 		{
 			var lst = _dbContext.Blogs.ToList();
 			return Ok(lst);
+
 		}
 
-		[HttpPost]
+        [HttpGet("{pageNo}/{pageSize}")]
+        public IActionResult GetBlogs(int pageNo, int pageSize)
+        {
+            // pageNo = 1 [ 1 - 10]
+            // pageNo = 2 [11 - 20]
+            // endRowNo = pageNo * pageSize; 10
+            // startRowNo = endRowNo - pageSize; 5670 - 10 = 5660 + 1 = 5661
+            // 567
+            var lst = _dbContext.Blogs
+                .Skip((pageNo - 1) * pageSize) // 2 - 1 = 1 * 10 = 10
+                .Take(pageSize)
+                .ToList();
+            var rowCount = _dbContext.Blogs.Count();
+            var pageCount = rowCount / pageSize; // 110 / 10 = 11
+            if (rowCount % pageSize > 0)
+            {
+                pageCount++;
+            }
+            return Ok(new
+            {
+                IsEndOfPage = pageNo >= pageCount,
+                PageCount = pageCount,
+                PageNo = pageNo,
+                PageSize = pageSize,
+                Data = lst
+            });
+        }
+
+        [HttpPost]
 		public IActionResult CreateBlogs()
 		{
 			return Ok("Post");
